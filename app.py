f44b4af1ee74ead7ad1747e97a1fae07d7697b30
sakinaelamri <elamri.sakina@icloud.com>
2025-04-07 22:59:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ pedigree = dpf_map[dpf_option]
 # Feature engineering with caps
 ins_gluc_ratio = insulin / glucose
 bmi_age_ratio = bmi / age
-bp_skin = min(bp * skinfold, 10)
-metabolic_stress = min(bmi * ins_gluc_ratio, 2)
+bp_skin = min(bp * skinfold, 3000)
+metabolic_stress = min(bmi * ins_gluc_ratio, )
 ins_age = min(insulin * age, 30)
-complex_stress = min((bmi * insulin) / (skinfold + 1), 1)
+complex_stress = min((bmi * insulin) / (skinfold + 1), 3000)
 
 st.markdown("---")
 if st.button("Estimate My Risk"):
